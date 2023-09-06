097907dab9526b48f36b5b3830f9fdc2bae5586a
cuiDarchan <cuisc16@mails.jlu.edu.cn>
2023-09-06 14:02:49 +0800

    fisrt commit: add three sensor driver

---
+++ hikvision_ros/build/CMakeFiles/hik_cam_node.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cui/workspace/livox_ws/src/hikvision_ros/node/hik_cam_node.cpp" "/home/cui/workspace/livox_ws/src/hikvision_ros/build/CMakeFiles/hik_cam_node.dir/node/hik_cam_node.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"hikvision_ros\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/opt/ros/noetic/include"
+  "/opt/ros/noetic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  "/usr/include/opencv4"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/cui/workspace/livox_ws/src/hikvision_ros/build/CMakeFiles/hik_cam.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hikvision_ros/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cui/workspace/livox_ws/src/hikvision_ros/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/opt/MVS/lib/64:/opt/MVS/lib/32:/opt/MVS/lib/64:/opt/MVS/lib/32:/opt/MVS/lib/64:/opt/MVS/lib/32'
+export PATH='/opt/ros/noetic/bin:~/anaconda3:/home/cui/anaconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/cui/workspace/livox_ws/src/hikvision_ros/build/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/cui/workspace/livox_ws/src/hikvision_ros:$ROS_PACKAGE_PATH"
